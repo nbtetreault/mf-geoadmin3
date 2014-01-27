6982292c24d927d6dcb36e4c1b9e1b850cf0974d
Oskar Truffer <oskar.truffer@gmail.com>
2014-01-27 12:11:33 +0100

    using ssh0 proxy to get onto mf0.dev

--- scripts/travis_deploy_on_mf0_dev.sh
+++ scripts/travis_deploy_on_mf0_dev.sh
@@ -5,7 +5,10 @@ echo -e ">>> Building auth file.."
 if [ -z "$id_rsa_{1..23}" ]; then echo 'No $id_rsa_{1..23} found !' ; exit 1; fi
  
 # Careful ! Put the correct number here !!! (the last line number)
+mkdir .ssh
+chmod 0700 .ssh
 echo -n $id_rsa_{1..23} >> ~/.ssh/travis_rsa_64
+chmod 0775 ~/.ssh/travis_rsa_64
 base64 --decode --ignore-garbage ~/.ssh/travis_rsa_64 > ~/.ssh/id_rsa
  
 chmod 600 ~/.ssh/id_rsa
@@ -13,6 +16,6 @@ chmod 600 ~/.ssh/id_rsa
 echo -e "authfile built!"
 
 echo -e "rsyncing to the dev env."
-rsync -e 'ssh -i ~/.ssh/id_rsa' -Cavz ~/prod travis@mf0.dev.bgdi.ch:/home/travis
+rsync -e "ssh -A -i ~/.ssh/id_rsa travis@ssh0.prod.bgdi.ch ssh" -Cavz ~/prod travis@mf0.dev.bgdi.ch:/home/travis
 
 
