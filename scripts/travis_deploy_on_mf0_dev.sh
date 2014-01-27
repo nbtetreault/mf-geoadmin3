e71c54753fe739a420895cf1e24adcc8e5949a54
Oskar Truffer <oskar.truffer@gmail.com>
2014-01-27 13:43:20 +0100

    debugging.

--- scripts/travis_deploy_on_mf0_dev.sh
+++ scripts/travis_deploy_on_mf0_dev.sh
@@ -7,17 +7,14 @@ if [ -z "$id_rsa_{1..23}" ]; then echo 'No $id_rsa_{1..23} found !' ; exit 1; fi
 # Careful ! Put the correct number here !!! (the last line number)
 mkdir .ssh
 chmod 0700 .ssh
-echo -n $id_rsa_{1..23} >> ~/.ssh/travis_rsa_64
+echo -n $id_rsa_{1..23} >> .ssh/travis_rsa_64
 chmod 0775 ~/.ssh/travis_rsa_64
-base64 --decode --ignore-garbage ~/.ssh/travis_rsa_64 > ~/.ssh/id_rsa
+base64 --decode --ignore-garbage .ssh/travis_rsa_64 > .ssh/id_rsa
  
-chmod 600 ~/.ssh/id_rsa
-
-cd ~/.ssh
-ls -al
+chmod 600 .ssh/id_rsa
 
 echo -e "authfile built!"
 
 echo -e "rsyncing to the dev env."
-rsync -e "ssh -A -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no travis@ssh0.prod.bgdi.ch ssh" -Cavz ~/geoadmin/mf-geoadmin3/prod travis@mf0.dev.bgdi.ch:/home/travis
+rsync -e "ssh -A -i .ssh/id_rsa -o StrictHostKeyChecking=no travis@ssh0.prod.bgdi.ch ssh" -Cavz ./prod travis@mf0.dev.bgdi.ch:/home/travis
 
